a52fbe7cf7933cef88c67da62db3fbe41d0cde1f
blgrimoussama <blgrimoussama@gmail.com>
2023-04-23 21:11:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ logging.basicConfig(level='DEBUG')
 
 logging.info('hello world')
 
+app = app
+
+logging.info(f'App: {app}')
+
 if __name__ == "__main__":
   app.secret_key = "my_secret_key"
   app.run(host='0.0.0.0', port=8080, debug=True)

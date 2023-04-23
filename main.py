0e9c9e0f8e5c2ae63c3fa9b37bb5a5a254a4255e
blgrimoussama <blgrimoussama@gmail.com>
2023-04-23 21:39:10 +0100

    app running method update

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from app.app import app
-import logging 
-logging.basicConfig(level='DEBUG')
+# from app.app import app
+# import logging 
+# logging.basicConfig(level='DEBUG')
 
-logging.info('hello world')
+# logging.info('hello world')
+
+# app = app
+
+# logging.info(f'App: {app}')
+
+# if __name__ == "__main__":
+#   app.secret_key = "my_secret_key"
+#   app.run(host='0.0.0.0', port=8080, debug=True)
+
+from app.app import app
 
 app = app
 
-logging.info(f'App: {app}')
 
 if __name__ == "__main__":
-  app.secret_key = "my_secret_key"
+  app.secret_key = "my_secret_key_Br41N_F@c3"
   app.run(host='0.0.0.0', port=8080, debug=True)
-
